73affbab285ba7d06fe17e6d95d8a4dafb28c21e
Víctor Rascón <vrascon.sedano89@gmail.com>
2021-05-16 18:28:26 +0200

    Creación Componentes

--- Blog/src/app/app.module.ts
+++ Blog/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
